d0d6113ac3d748969b312309542615ed4f6576cb
yubin <641818243@qq.com>
2017-05-27 14:21:06 +0800

    kafka offset lag查询小工具

---
+++ README.md
@@ -0,0 +1,13 @@
+# kafka offset的lag监控小工具，支持传参
+
+## ./kafka_offset_monitor -zklist="localhost:2181" -topic="xxxx" -consumer="yyyy"
+
+
+2017/05/27 14:08:15 Connected to [::1]:2181
+2017/05/27 14:08:15 Authenticated: id=97941928563775014, timeout=5000
+2017/05/27 14:08:15 Re-submitting `0` credentials after reconnect
+yyyy: 0
+2017/05/27 14:08:15 Recv loop terminated: err=EOF
+2017/05/27 14:08:15 Send loop terminated: err=<nil>
+
+
---
+++ kafka_offset_monitor.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	zk "github.com/samuel/go-zookeeper/zk"
+	"os"
+	"strings"
+	"time"
+	"strconv"
+)
+
+var (
+	zklist     string
+	consumer   string
+	topic      string
+	old_offset int
+	new_offset int
+	lag        int
+)
+
+func main() {
+	flag.StringVar(&zklist, "zklist", "localhost:2181", "zookeeperlist")
+	flag.StringVar(&consumer, "consumer", "uic-comsumer-group-test", "consumer")
+	flag.StringVar(&topic, "topic", "uic-topic", "topic")
+
+	flag.Parse()
+
+	old_offset = 0
+	new_offset = 0
+
+	zkconn, _, err := zk.Connect(strings.Split(zklist, ","), 5*time.Second)
+
+	defer zkconn.Close()
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
+
+	topic_path := "/consumers/" + consumer + "/" + "offsets" + "/" +topic
+
+	partition, _, err := zkconn.Children(topic_path)
+
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
+
+	for _,offset_d := range partition {
+		rs_byte,_,_ := zkconn.Get(topic_path + "/" + offset_d)
+		rs_int,_ := strconv.Atoi(string(rs_byte[:]))
+		old_offset += rs_int
+	}
+	time.Sleep(1)
+	for _,offset_d := range partition {
+		rs_byte,_,_ := zkconn.Get(topic_path + "/" + offset_d)
+		rs_int,_ := strconv.Atoi(string(rs_byte[:]))
+		new_offset += rs_int
+	}
+
+	lag = new_offset - old_offset
+	fmt.Println(topic + ":",lag)
+
+
+}
